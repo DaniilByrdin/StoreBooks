f2951001d59f4bcc5994a05f83471d58a2e7a377
DaniilByrdin <byrdin0202@mail.ru>
2023-08-13 09:40:46 +0300

    adding styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,17 +3,21 @@ import React from "react";
 import './app.css'
 import { Route, Routes } from "react-router";
 
-import HomePage  from '../pages/homePage'
+import HomePage from '../pages/homePage'
 import CardPage from '../pages/cardPage'
+import ShopHeader from "../shop-header/shop-header";
 
-const App = ( ) => {
+const App = () => {
 
     return (
-        <Routes>
-            <Route path="/" exact Component={HomePage} />
-            <Route path="/card" Component={CardPage} />
-        </Routes>
+        <main role="main" className="container">
+            <ShopHeader numItems={5} total={210} />
+            <Routes>
+                <Route path="/" exact Component={HomePage} />
+                <Route path="/card" Component={CardPage} />
+            </Routes>
+        </main>
     )
 }
 
-export default  App;
+export default App;
--- src/components/book-list-item/book-list-irem.js
+++ src/components/book-list-item/book-list-irem.js
@@ -4,13 +4,20 @@ import './book-list-item.css'
 
 const BookListItem = ({ book }) => {
 
-    const { author, title } = book
+    const { author, title, price, coverImage } = book
 
     return (
-        <Fragment>
-            <span> { author } </span>
-            <span> { title } </span>
-        </Fragment>
+        <div className="book-list-item">
+            <div className="book-cover">
+                <img src={coverImage} alt="cover"/ >
+            </div>
+            <div className="book-details">
+                <a href="#" className="book-title" > {author} </a>
+                <div className="book-aithor"> {title} </div>
+                <div className="book-price"> {price} </div>
+                <button className="btn btn-info add-to-cart">Add to cart</button>
+            </div>
+        </div>
     )
 }
 
